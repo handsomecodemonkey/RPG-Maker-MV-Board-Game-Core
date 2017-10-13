e6fa0840cc41aa8fa784a40fe69326bac84892d9
Erick Kusnadi <erick@handsomecodemonkey.com>
2017-10-13 16:31:06 -0700

    fixed board class declaration issue

--- Board Game Core/js/plugins/BoardGameCore.js
+++ Board Game Core/js/plugins/BoardGameCore.js
@@ -10,8 +10,64 @@
  *
  */
 
-(function() {
+ /* Board Data Model */
+
+    //-----------------------------------------------------------------------------
+	// Board_Space
+	//
+	// A representation of a space on the board
+	
+	function Board_Space() {
+	    this.initialize.apply(this, arguments);
+	}
+
+	Board_Space.prototype.initialize = function() {
+	    this.initMembers();
+	};
+	
+	Board_Space.prototype.initMembers = function() {
+	    this._xCoord = 0;
+	    this._yCoord = 0;
+	    this._directionsToNextSpace = [];
+	};
+
+	Board_Space.prototype.setXY = function(x,y) {
+		this._xCoord = x;
+		this._yCoord = y;
+	};
+
+	//-----------------------------------------------------------------------------
+	// Board_Model
+	//
+	// A representation of a space on the board
+ 
+	function Board_Model() {
+	    this.initialize.apply(this, arguments);
+	}
+
+	Board_Model.prototype.initialize = function() {
+	    this.initMembers();
+	};
+
+	Board_Model.prototype.initMembers = function() {
+	    this._boardSpaces = [];
+	};
+
+	Board_Model.prototype.addSpace = function(space) {
+		this._boardSpaces.push(space);
+	};
+
+	Board_Model.prototype.addSpaces = function(spaces) {
+		for(var i = 0; i < spaces.length; i++){
+			this.addSpace(spaces[i]);
+		}
+	};
+
+/* End Board Data Model */
     
+
+/* Main Plugin Function */
+(function() {
     /* Setup */
 
     //Get parameters from plugin manager
@@ -19,9 +75,20 @@
     var test = String(parameters['test']);
 
     //setupBoardGame
+    var $boardMap = new Board_Model();
+
     function setupBoardGame() {
     	$gameSystem.disableEncounter();
     	//Game_Player.prototype.canMove = function() { return false; };
+
+    	//Test map
+    	
+    	for(var i = 8; i != 0; i--) {
+    		var newSpace = new Board_Space();
+    		newSpace.setXY(0,i);
+    		$boardMap.addSpace(newSpace);
+    	}
+    	
     }
 
     //TODO: Load saved data
@@ -90,5 +157,5 @@
     function diceRoll(from, to) {
         return Math.floor(to * Math.random()) + from;
     }
-
+	
 })();
